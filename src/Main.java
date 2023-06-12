9f961eb8ae832b721ed52a05cf490481339f6a47
Rednap <wmicer@gmail.com>
2023-06-12 19:20:15 +0300

    done task!

--- src/Main.java
+++ src/Main.java
@@ -9,12 +9,18 @@ public class Main {
         File file = new File("D:\\romeo-and-juliet.txt");    //"D:\\romeo-and-juliet.txt"
         FileReader fileReader = new FileReader(file);
         String wordWithMaxLength = "";
-        String[] words = fileReader.toString().split("(_|\\s)+");
-        while (fileReader.read() != -1) {
+        StringBuilder line = new StringBuilder();
+
+        int a;
+        while ((a = fileReader.read()) != -1) {
+            line.append((char) a);
+        }
+        fileReader.close();
+        String[] words = line.toString().split("(\\W)+");
+        for (int i = 0; i < words.length; i++) {
             Arrays.sort(words, Comparator.comparing(String::length, Comparator.reverseOrder()));
             wordWithMaxLength = words[0].length() > wordWithMaxLength.length() ? words[0] : wordWithMaxLength;
         }
-        fileReader.close();
         System.out.println(wordWithMaxLength + '\n' + wordWithMaxLength.length() + " chars");
     }
 }
